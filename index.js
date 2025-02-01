30cf6fdd0fae0b430e110b926e33c1444ec6aae2
murtozayev <jahongirmurtozayev777@gmail.com>
2025-02-01 17:00:21 +0500

    COMP

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://nibbleapp.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
