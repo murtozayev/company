5bb1d5408f611cfa0e9f6432f5b23e1ce94e044a
murtozayev <jahongirmurtozayev777@gmail.com>
2025-02-01 20:38:13 +0500

    Update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://companyapp-delta.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
